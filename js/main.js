36c9defa62f2635bb593fe0216dd9ead4c1f4641
AKS-RU <almetman.aks@gmail.com>
2025-09-16 23:37:55 +0300

    Добавлен блок  style со слайдером

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  initialSlide: 1,
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
